9c846c0c5d537d80003a84b6bf3aa0c74847158e
AyoolaVictor <ayoolavictor415@gmail.com>
2023-11-20 17:55:50 +0100

    Updated the deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports={
-    solidity:"0.8.19",
+    solidity:"0.8.20",
     networks:{
         sepolia:{
             url:process.env.Api_key,
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,10 +14,20 @@ const main=async() =>{
   await TESTUSDT.waitForDeployment();
   console.log("testUSDT deployed to:",await TESTUSDT.getAddress())
 
-  // second contract
+  //second contract
+  const testWBTC = await hre.ethers.getContractFactory("testWBTC");
+  const TESTWBTC = await testWBTC.deploy("testWBTC","WBTC","1000000000000000000000");
+
+  await TESTWBTC.waitForDeployment();
+  console.log("testWBTC deployed to:",await TESTWBTC.getAddress())
+
+
+
+
+  // third contract
   
   const paymentGateway = await hre.ethers.getContractFactory("Payblauq_Merchant");
-  const PAYMENTGATEWAY = await paymentGateway.deploy(await TESTUSDT.getAddress());
+  const PAYMENTGATEWAY = await paymentGateway.deploy("0xf04990915C006A35092493094B4367F6d93f9ff0",await TESTUSDT.getAddress(),await TESTWBTC.getAddress());
 
   await PAYMENTGATEWAY.waitForDeployment();
   console.log("Payment Gateway deployed to:",await PAYMENTGATEWAY.getAddress())
